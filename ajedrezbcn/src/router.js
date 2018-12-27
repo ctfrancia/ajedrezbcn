12b127871498e03d973d94efa77605f36b0a111d
Christian Francia <CTFrancia@gmail.com>
2018-12-27 18:38:19 +0100

    add views to router

--- ajedrezbcn/src/router.js
+++ ajedrezbcn/src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import Tournaments from './views/Tournaments.vue';
+import Login from './views/Login.vue';
 
 Vue.use(Router);
 
@@ -19,5 +21,16 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
     },
+    {
+      path: '/torneos',
+      name: 'tournaments',
+      component: Tournaments,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+    }
+
   ],
 });
